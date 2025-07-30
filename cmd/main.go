99d3e1400cd0b3dba70a4c3999db344ff8fbb9c1
yumee2 <miyazaki1707@gmail.com>
2025-07-30 15:36:15 +0700

    logger has been added

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"url_shortener/internal/config"
 
 	"github.com/joho/godotenv"
 )
 
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
 func main() {
 	godotenv.Load() // load dotenv file
 	cfg := config.MustLoad()
+
+	logger := createLogger(cfg.Env)
+	logger.Info("application has been started")
+}
+
+func createLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
